f5898eb3302a15c5b4c81f74034fddb81a94805a
P@rth Roy <royparth94@yahoo.com>
2023-12-06 17:02:38 +0530

    add test index.html

--- utils.js
+++ utils.js
@@ -10,7 +10,7 @@ const fs = require("fs");
 // Combine the drive letter, folder name, and file name to create the full file path
 const driveLetter = "C:"; // Replace with the drive letter you want to write to
 // Combine the drive letter, folder name, and file name to create the full file path
-const folderPath = path.join(driveLetter, "xampp", "htdocs", "ari");
+const folderPath = path.join(driveLetter, "ari");
 
 // Define your API endpoint and API key
 const apiUrl =
@@ -33,86 +33,88 @@ function generateRandomString(length) {
 
 let isTTSPending = false;
 
-const handleTTS = (text, lang, fileStream) => {
+const handleTTS = async (text, lang, fileStream) => {
   isTTSPending = true;
-
+  console.log("handleTTS");
   function linearInterpolate(sample1, sample2, fraction) {
     return sample1 * (1 - fraction) + sample2 * fraction;
   }
-
-  axios
-    .post(
-      "http://183.82.10.250:6631/tts_stream",
-      {
+  try {
+    let config = {
+      method: "post",
+      // maxBodyLength: Infinity,
+      url: "http://183.82.10.250:6631/tts_stream",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
         text: text,
         language: lang,
         gpt_cond_latent: clone_speaker.gpt_cond_latent,
         speaker_embedding: clone_speaker.speaker_embedding,
         add_wav_header: false,
+        stream_chunk_size: "20",
       },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        responseType: "stream",
-      }
-    )
-    .then((response) => {
-      //   if (!response.ok) {
-      //     throw new Error("Network response was not ok", response);
-      //   }
-
-      response.data.pipe(fileStream);
-
-      //   const reader = response.body.on("data", processAudioChunk);
-      //   let audioQueue = [];
-      //   let isStreamingFinished = false;
-      //   let nextSample = 0;
-      //   let amplitudeSum = 0; // Accumulator for amplitude values
-      //   let sampleCount = 0; // Counter for number of samples processed
-
-      //   function processAudioChunk(value) {
-      //     // Convert the incoming data to Int16Array and add it to the queue
-      //     const rawData = new Int16Array(
-      //       value.buffer,
-      //       value.byteOffset,
-      //       value.byteLength / 2
-      //     );
-      //     audioQueue = audioQueue.concat(Array.from(rawData));
-
-      //     // reader.then(processAudioChunk);
-      //   }
-      //   response.body.on("end", () => {
-      //     isStreamingFinished = true;
-      //     // Create a WAV file header
-      //     const header = Buffer.alloc(44);
-      //     header.write("RIFF", 0);
-      //     header.writeUInt32LE(36 + int16Array.length * 2, 4); // Total file size
-      //     header.write("WAVE", 8);
-      //     header.write("fmt ", 12);
-      //     header.writeUInt32LE(16, 16); // PCM format
-      //     header.writeUInt16LE(1, 20); // Audio format (1 for PCM)
-      //     header.writeUInt16LE(1, 22); // Number of channels (1 for mono)
-      //     header.writeUInt32LE(44100, 24); // Sample rate (e.g., 44.1 kHz)
-      //     header.writeUInt32LE(44100 * 2, 28); // Byte rate (sample rate * block align)
-      //     header.writeUInt16LE(2, 32); // Block align (number of bytes per sample)
-      //     header.writeUInt16LE(16, 34); // Bits per sample
-      //     header.write("data", 36);
-      //     header.writeUInt32LE(int16Array.length * 2, 40); // Size of the data chunk
-
-      //     // Concatenate the header and the Int16Array
-      //     const wavData = Buffer.concat([header, Buffer.from(int16Array.buffer)]);
-      //     fileStream.write(wavData);
-      //     console.log("fileStream", fileStream);
-      //     return;
-      //   });
-
-      //   reader.then(processAudioChunk);
-    })
-    .catch((error) => {
-      console.error("Error calling TTS service", error);
+    };
+    const response = await axios(config);
+    if (!response.status === 200 || !response.data) {
+      console.log("error in tts");
       return;
-    });
+      // throw new Error(response.statusText);
+    }
+    // const fileName = generateRandomString(10);
+
+    // const outputPath = path.join(folderPath, fileName + ".wav");
+    // const fileStream1 = fs.createWriteStream(outputPath);
+
+    //   if (!response.ok) {
+    //     throw new Error("Network response was not ok", response);
+    //   }
+    // response.data.pipe(fileStream);
+    // const buffer = Buffer.from(response.data);
+    // const reader = new Wav.Reader();
+    // reader.end(buffer);
+    // reader.pipe(fileStream);
+
+    // fileStream.write(buffer);
+
+    console.log("streaming........");
+    // response.data.on("data", async (chunk) => {
+    //   fileStream.write(chunk);
+    // });
+    // fs.writeFileSync(filePath, audioData, { flag: 'a' });
+    // response.data.pipe(fileStream1);
+    // fs.writeFileSync(outputPath, response.data, { flag: 'a' });
+
+    // Pipe the received stream to the writable stream
+    // response.data.pipe(fileStream);
+
+    // Event handlers for stream events
+    // response.data.on("end", () => {
+    //   console.log("Audio download completed.");
+    // });
+
+    // response.data.on("error", (err) => {
+    //   console.error("Error downloading audio:", err);
+    // });
+    // response.data.on("data", async (chunk) => {
+    fileStream.write(response.data);
+    // });
+    // response.data.on("end", () => {
+    //   // Handle the end of the stream
+    //   fileStream.end();
+    // });
+    // Wait for the stream to finish writing to the file
+    // await new Promise((resolve, reject) => {
+    //   fileStream.on("finish", resolve);
+    //   fileStream.on("error", reject);
+    // });
+
+    console.log("response data", response.data);
+  } catch (error) {
+    console.error("Error calling TTS service", error);
+    return;
+  }
 };
 
 const generateBotResponse = async (text, lang) => {
@@ -121,7 +123,7 @@ const generateBotResponse = async (text, lang) => {
   try {
     const fileName = generateRandomString(10);
 
-    const outputPath = path.join(folderPath, fileName + ".wav");
+    const outputPath = path.join(folderPath, fileName + ".mp3");
     const fileStream = fs.createWriteStream(outputPath);
 
     const response = await axios.post(
@@ -190,8 +192,12 @@ const generateBotResponse = async (text, lang) => {
     });
 
     response.data.on("end", () => {
+      console.log("end of llm respose");
       // Handle the end of the stream
-      fileStream.end();
+      // fileStream.end();
+    });
+    fileStream.on("close", () => {
+      console.log(`Audio saved to ${outputPath}`);
     });
   } catch (error) {
     console.error("Error calling generate_stream service:", error.message);
